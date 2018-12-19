3177044cb2bafca8660c167b7a7315547862e731
MFRhodriH <42868257+MFRhodriH@users.noreply.github.com>
2018-12-19 15:04:38 +0000

    A bootstrap instance is now brought up to configure the domain and DB. There is now the option of starting 2 ES instances. The Demo regions are automatically started under the ESDemoUser if they are installed (#3)

---
+++ scripts/StartESRegion.ps1
@@ -0,0 +1,15 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$RegionName
+)
+
+try {
+    $ErrorActionPreference = "Stop"
+     Start-Transcript -Path c:\cfn\log\$($MyInvocation.MyCommand.Name).txt -Append -IncludeInvocationHeader;
+	& "C:\Program Files (x86)\Micro Focus\Enterprise Server\bin\casstart.exe" `
+		"-r$RegionName"
+
+} catch {
+      $_ | Write-AWSQuickStartException
+}
